57bd00d61fae67fe111eea37af22da6b3d86fdc5
Dilmurod Bukharov <dilmurod.bukharov@gmail.com>
2025-02-06 22:56:50 +0500

    refactor: move the state and functions
    Move the state declaration with default values and functions from Content component to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import './App.css';
+import { useState } from 'react'
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
 
 function App() {
 
+  const [items, setItems] = useState([
+    {
+        id: 1,
+        checked: false,
+        item: "Das Brot"
+    },
+    {
+        id: 2,
+        checked: true,
+        item: "Der Wein"
+    },
+    {
+        id: 3,
+        checked: false,
+        item: "Der Kuchen"
+    },
+    {
+        id: 4,
+        checked: false,
+        item: "Die Birne"
+    }
+])
+
+const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
+    setItems(listItems)
+    localStorage.setItem('grocerylist', JSON.stringify(listItems))
+}
+
+const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('grocerylist', JSON.stringify(listItems))
+}
+
   return (
     <div className="App">
-      <Header />
-      <Content />
+      <Header title = "Grocery" />
+      <Content 
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete} />
       <Footer />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -1,45 +1,7 @@
 import React from 'react'
-import { useState } from 'react'
 import { FaTrashAlt } from 'react-icons/fa'
 
-const Content = () => {
-
-    const [items, setItems] = useState([
-        {
-            id: 1,
-            checked: false,
-            item: "Das Brot"
-        },
-        {
-            id: 2,
-            checked: true,
-            item: "Der Wein"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Der Kuchen"
-        },
-        {
-            id: 4,
-            checked: false,
-            item: "Die Birne"
-        }
-    ])
-
-    const handleCheck = (id) => {
-        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
-        setItems(listItems)
-        localStorage.setItem('grocerylist', JSON.stringify(listItems))
-    }
-
-    const handleDelete = (id) => {
-        const listItems = items.filter((item) => item.id !== id)
-        setItems(listItems)
-        localStorage.setItem('grocerylist', JSON.stringify(listItems))
-    }
-
-
+const Content = ({ items, handleCheck, handleDelete }) => {
 
   return (
     <main>
